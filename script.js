336db3958dea01ce277749c8d0894bd7d8ed97d1
richardtli <43454832+richardtli@users.noreply.github.com>
2023-05-04 19:46:06 -0500

    sdfsdf

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const info = document.querySelector(".info");
+document.querySelector(".permission").addEventListener('onclick', () => {
+    askPermission()
+})
 
-DeviceOrientationEvent.requestPermission().then(response => {
-                    if (response == 'granted') {window.addEventListener(
-                        "deviceorientation",
-                        (event) => {
-                          const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-                          const leftToRight = event.gamma; // gamma: left to right
-                          const frontToBack = event.beta; // beta: front back motion
-                      
-                          handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-                        },
-                        true
-                      );
-                    }
-                }
-                )
+function askPermission() {
+  DeviceOrientationEvent.requestPermission().then((response) => {
+    if (response == "granted") {
+      window.addEventListener(
+        "deviceorientation",
+        (event) => {
+          const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+          const leftToRight = event.gamma; // gamma: left to right
+          const frontToBack = event.beta; // beta: front back motion
+          handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+        },
+        true
+      );
+    }
+  });
+}
 
 
 const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
