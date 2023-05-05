d9b3f2096a98e12cba4aad64356216f38ee6b5d0
richardtli <43454832+richardtli@users.noreply.github.com>
2023-05-04 23:32:47 -0500

    asdsad

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function askPermission() {
           const beta = Math.floor(event.beta); // beta: front back motion
           let rel_beta = beta - first_beta
           let rel_gamma = gamma - first_gamma
-          if(beta < 90 && beta > -90) {
+          if(beta < 86 && beta > -86) {
             handleOrientationEvent(rel_gamma, rel_beta);
           }
           FIRST_ITER = false;
