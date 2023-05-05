739091821445553d73e993e0457474244c53e4f4
richardtli <43454832+richardtli@users.noreply.github.com>
2023-05-04 19:40:55 -0500

    sfd

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const info = document.querySelector(".info");
 
-window.addEventListener(
-  "deviceorientation",
-  (event) => {
-    const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-    const leftToRight = event.gamma; // gamma: left to right
-    const frontToBack = event.beta; // beta: front back motion
+DeviceOrientationEvent.requestPermission().then(response => {
+                    if (response == 'granted') {window.addEventListener(
+                        "deviceorientation",
+                        (event) => {
+                          const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+                          const leftToRight = event.gamma; // gamma: left to right
+                          const frontToBack = event.beta; // beta: front back motion
+                      
+                          handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+                        },
+                        true
+                      );
+                    }
+                }
+                )
 
-    handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-  },
-  true
-);
 
 const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
   info.innerText = `Alpha: ${rotateDegrees}, Beta: ${frontToBack}, Gamma: ${leftToRight}`;
