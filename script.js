2827c5d447ad6a51228b97b55f5c9e4d183c8159
richardtli <43454832+richardtli@users.noreply.github.com>
2023-05-04 19:39:29 -0500

    sdfsdf

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-const info = document.querySelector(".info")
+const info = document.querySelector(".info");
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener(
-      "deviceorientation",
-      (event) => {
-        const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-        const leftToRight = event.gamma; // gamma: left to right
-        const frontToBack = event.beta; // beta: front back motion
-  
-        //handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-      },
-      true
-    );
-  }
-  
-  const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
-    info.innerText = `Alpha: ${rotateDegrees}, Beta: ${frontToBack}, Gamma: ${leftToRight}`
-  };
+window.addEventListener(
+  "deviceorientation",
+  (event) => {
+    const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+    const leftToRight = event.gamma; // gamma: left to right
+    const frontToBack = event.beta; // beta: front back motion
 
-  info.innerText = window.DeviceOrientationEvent
+    handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+  },
+  true
+);
+
+const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
+  info.innerText = `Alpha: ${rotateDegrees}, Beta: ${frontToBack}, Gamma: ${leftToRight}`;
+};
